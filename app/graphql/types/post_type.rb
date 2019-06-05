e17140fe761d77ae1aaafd093d120517912efa11
harhogefoo <harimanmon@gmail.com>
2019-06-05 17:29:25 +0900

    feat: postの削除

--- app/graphql/types/post_type.rb
+++
@@ -1,7 +0,0 @@
-module Types
-  class PostType < GraphQL::Schema::Object
-    description "A blog post"
-    field :id, ID, null: false
-    field :title, String, null: false
-  end
-end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,22 +1,9 @@
-require_relative "post_type"
-require_relative "../../models/post"
 require_relative "memo_type"
 require_relative "../../models/memo"
 
 module Types
   class QueryType < GraphQL::Schema::Object
     description "The query root of this schema"
-
-    # First describe the field signature:
-    field :post, PostType, null: true do
-      description "Find a post by ID"
-      argument :id, ID, required: true
-    end
-
-    # Then provide an implementation:
-    def post(id:)
-      Post.find(id)
-    end
 
     field :memos, [MemoType], null: true do
       description "Find all memo"
