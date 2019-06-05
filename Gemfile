67842bc52356c6afc3845e8aa24a918864751460
harhogefoo <harimanmon@gmail.com>
2019-06-05 16:04:03 +0900

    feat: setup graphql

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+gem 'aws-record', '~> 2'
+
+gem 'graphql'
--- handler.rb
+++ handler.rb
@@ -1,11 +1,28 @@
 require 'json'
-
-def hello(event:, context:)
-  { statusCode: 200, body: JSON.generate('Go Serverless v1.0! Your function executed successfully!') }
-end
+require_relative 'app/graphql/schema'
 
 def graphql(event:, context:)
+  body = JSON.parse(event["body"])
+  query = body["query"]
+  variables = body["variables"]
+  operation_name = body["operationName"]
+
+  body = Schema.execute(
+    query: body["query"],
+    variables: body["variables"],
+    operation_name: body["operationName"]
+  )
+
   {
-    statusCode: 200, body: JSON.generate('This is GraphQL ruby')
+    statusCode: 200,
+    body: JSON.generate(body)
+  }
+rescue StandardError => e
+  puts e.message
+  puts e.backtrace.inspect
+
+  {
+    statusCode: 400,
+    body: JSON.generate("Bad request, please POST a request body!")
   }
 end
